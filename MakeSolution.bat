e10ec66074eef6363fb478406738e0ab8fc0421b
T8 <a@a.com>
2025-04-16 17:18:00 +0800

    Update CMakeList.txt, add DirectXMath static library.

--- MakeSolution.bat
+++ MakeSolution.bat
@@ -10,15 +10,15 @@ echo.
 if "%CLEAN%"=="true" (
     echo Deleting the old CMake files...
     rd /s/q Cmake
-)
-else (
+) else (
     echo Tip: You can use "-c" to forcely clean the old CMake files.
 )
 echo.
 
 :: Generate.
-cmake -S ./SourceCode -B ./Cmake -G "Visual Studio 17 2022"
+cmake -S ./SourceCode -B ./Cmake
 
 :: Open the solution.
+echo.
 set /p input=Open the solution? (y/n)
 if /i "%input%"=="y" start ./Cmake/CrucibleSolution.sln
