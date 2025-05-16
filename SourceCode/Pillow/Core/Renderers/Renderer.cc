6d966bedfe4b0f14504fac29dfc21066ce93b1cc
IceyPillow <ua7t@outlook.com>
2025-05-16 09:39:08 +0800

    Implement the generic renderer

---
+++ SourceCode/Pillow/Core/Renderers/Renderer.cc
@@ -0,0 +1,68 @@
+#include "Renderer.h"
+
+namespace
+{
+   std::vector<std::thread> workers;
+   void BarrierCompletionAction() noexcept;
+   std::optional<std::barrier<void(*)() noexcept>> frameBarrier;
+   std::atomic<bool> signalActive;
+   std::atomic<bool> signalSync;
+
+   void BarrierCompletionAction() noexcept
+   {
+      signalSync.store(false, std::memory_order::release);
+   }
+}
+
+Pillow::GenericRenderer::GenericRenderer(int32_t threadCount, std::string name) :
+   _RendererName(name),
+   _ThreadCount(threadCount)
+{
+   workers.reserve(threadCount);
+   frameBarrier.emplace(threadCount, BarrierCompletionAction);
+   signalActive.store(true);
+   signalSync.store(false);
+}
+
+Pillow::GenericRenderer::~GenericRenderer()
+{
+   workers.clear();
+   frameBarrier.reset();
+}
+
+void Pillow::GenericRenderer::RendererLaunch()
+{
+   for (int32_t i = 0; i < _ThreadCount; i++)
+   {
+      workers.emplace_back(std::thread(&GenericRenderer::BaseWorker, this, i));
+   }
+}
+
+void Pillow::GenericRenderer::RendererTerminate()
+{
+   signalActive.store(false);
+   for (auto& thread : workers)
+   {
+      if (thread.joinable()) thread.join();
+   }
+}
+
+void Pillow::GenericRenderer::FrameBegin()
+{
+   signalSync.store(true, std::memory_order::release);
+}
+
+bool Pillow::GenericRenderer::IsFrameEnd()
+{
+   return !signalSync.load(std::memory_order::acquire);
+}
+
+void Pillow::GenericRenderer::BaseWorker(int32_t workerIndex)
+{
+   while (signalActive.load())
+   {
+      while (!signalSync.load(std::memory_order::acquire)) std::this_thread::yield();
+      this->Worker(workerIndex);
+      frameBarrier->arrive_and_wait();
+   }
+}
---
+++ SourceCode/Pillow/Core/Renderers/Renderer.h
@@ -0,0 +1,85 @@
+#pragma once
+#include <thread>
+#include <barrier>
+#include <atomic>
+#include <vector>
+#include <functional>
+#include "../Auxiliaries.h"
+#include "../AppConstants.h"
+#include "../Texture.h"
+
+namespace Pillow
+{
+   enum class GenericRendererResourceType : int32_t
+   {
+      Mesh = 1 << 28,
+      Texture = 2 << 28,
+      PiplelineState = 3 << 28,
+      ConstantBuffer = 4 << 28,
+   };
+
+   ForceInline GenericRendererResourceType GetResType(int32_t handle)
+   {
+      return GenericRendererResourceType(handle & (7 << 28));
+   }
+
+   class GenericRenderer
+   {
+      DeleteDefautedMethods(GenericRenderer)
+         ReadonlyProperty(std::string, RendererName)
+         ReadonlyProperty(int32_t, ThreadCount)
+   
+   public:
+      virtual ~GenericRenderer() = 0;
+      virtual int32_t CreateMesh() = 0;
+      virtual int32_t CreateTexture() = 0;
+      virtual int32_t CreateConstantBuffer() = 0;
+      virtual int32_t CreatePiplelineState() = 0;
+      virtual void ReleaseResource(int32_t handle) = 0;
+      void RendererLaunch();
+      void RendererTerminate();
+      void FrameBegin();
+      bool IsFrameEnd();
+
+   protected:
+      GenericRenderer(int32_t threadCount, std::string name);
+      virtual void Worker(int32_t workerIndex) = 0;
+   private:
+      void BaseWorker(int32_t workerIndex);
+   };
+
+#if defined(_WIN64)
+   class D3D12Renderer : public GenericRenderer
+   {
+      DeleteDefautedMethods(D3D12Renderer)
+   
+   public:
+      D3D12Renderer(HWND windowHandle, int32_t threadCount);
+      ~D3D12Renderer() override;
+      int32_t CreateMesh() override;
+      int32_t CreateTexture() override;
+      int32_t CreatePiplelineState() override;
+      int32_t CreateConstantBuffer() override;
+      void ReleaseResource(int32_t handle) override;
+   protected:
+      void Worker(int32_t workerIndex) final;
+   };
+
+#elif defined(__ANDROID__)
+   //class GLES32Renderer : public GenericRenderer
+   //{
+   //   DeleteDefautedMethods(GLES32Renderer)
+   //public:
+   //   GLES32Renderer(HWND windowHandle);
+   //   ~GLES32Renderer();
+   //   int32_t CreateMesh() override;
+   //   int32_t CreateTexture() override;
+   //   int32_t CreatePiplelineState() override;
+   //   int32_t CreateConstantBuffer() override;
+   //   void ReleaseResource(int32_t handle) override;
+   //   void CPUFrameBegin() override;
+   //   void CPUFrameEnd() override;
+   //};
+
+#endif
+}
