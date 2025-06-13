8084b4c74c3870a401b67b3641a12e8aed40fc7d
IceyPillow <ua7t@outlook.com>
2025-06-13 19:34:40 +0800

    Revise the generic texture formats

--- SourceCode/Pillow/Core/Texture.h
+++ SourceCode/Pillow/Core/Texture.h
@@ -7,19 +7,19 @@ namespace Pillow::Graphics
 {
    enum class GenericTextureFormat : int8_t
    {
-      UNORM_R8G8B8A8,
-      UNORM_R8G8B8,
-      UNORM_R8G8,
-      UNORM_R8,
+      // 1.Supports .hdr files.
+      // 2.R8G8B8 isn't supported in DXGI_FORMAT, use R8G8B8A8 to store it.
+      UnsignedNormalized_R8G8B8A8,
+      UnsignedNormalized_R8G8,
+      UnsignedNormalized_R8,
       Count
    };
 
    const int32_t PixelSize[(int32_t)GenericTextureFormat::Count]
    {
-      4, // UNORM_R8G8B8A8
-      3, // UNORM_R8G8B8
-      2, // UNORM_R8G8
-      1, // UNORM_R8
+      4, // UnsignedNormalized_R8G8B8A8
+      2, // UnsignedNormalized_R8G8
+      1, // UnsignedNormalized_R8
    };
 
    class GenericTextureInfo
