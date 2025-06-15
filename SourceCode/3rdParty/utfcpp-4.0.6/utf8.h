6849a0bcb04e474d788a165d1da9bd6a286bfb07
IceyPillow <ua7t@outlook.com>
2025-06-15 09:29:50 +0800

    Enable the compilation option /Zc:__cplusplus

--- SourceCode/3rdParty/utfcpp-4.0.6/utf8.h
+++ SourceCode/3rdParty/utfcpp-4.0.6/utf8.h
@@ -39,9 +39,7 @@ Some library features will be disabled.
 
 If you leave UTF_CPP_CPLUSPLUS undefined, it will be internally assigned to __cplusplus.
 */
-#ifndef UTF_CPP_CPLUSPLUS
-#define UTF_CPP_CPLUSPLUS 202002L
-#endif
+// Pillow: Now __cplusplus always returns an updated value.
 
 #include "utf8/checked.h"
 #include "utf8/unchecked.h"
