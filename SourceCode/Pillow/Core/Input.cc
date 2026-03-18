3bebd11a1a04f5c75ad1f4d4f4891f85dbdb378c
Pillow's Lag Laptop <109895040+ModulePillow@users.noreply.github.com>
2026-03-18 23:45:37 +0100

    Add KeyMap; revise some code

--- SourceCode/Pillow/Core/Input.cc
+++ SourceCode/Pillow/Core/Input.cc
@@ -26,6 +26,127 @@ namespace
       Down,
       Up
    };
+
+#if defined(_WIN64)
+   const char TriggerThreshold = XINPUT_GAMEPAD_TRIGGER_THRESHOLD;
+
+   const std::map<uint16_t, GenricKey> KeyMap =
+   {
+      // Mouse Buttons
+      {VK_LBUTTON, GenricKey::MiceLeft},
+      {VK_MBUTTON, GenricKey::MiceMiddle},
+      {VK_RBUTTON, GenricKey::MiceRight},
+      {VK_XBUTTON1, GenricKey::MiceSide0},
+      {VK_XBUTTON2, GenricKey::MiceSide1},
+      // GamePad Buttons (XInput)
+      // GamePad Triggers are analog buttons, so we need to check their values.
+      {XINPUT_GAMEPAD_A, GenricKey::PadA},
+      {XINPUT_GAMEPAD_B, GenricKey::PadB},
+      {XINPUT_GAMEPAD_X, GenricKey::PadX},
+      {XINPUT_GAMEPAD_Y, GenricKey::PadY},
+      {XINPUT_GAMEPAD_DPAD_UP, GenricKey::PadUp},
+      {XINPUT_GAMEPAD_DPAD_DOWN, GenricKey::PadDown},
+      {XINPUT_GAMEPAD_DPAD_LEFT, GenricKey::PadLeft},
+      {XINPUT_GAMEPAD_DPAD_RIGHT, GenricKey::PadRight},
+      // LT and RT are linear triggers, must be treated separately.
+      {XINPUT_GAMEPAD_LEFT_SHOULDER, GenricKey::PadLB},
+      {XINPUT_GAMEPAD_RIGHT_SHOULDER, GenricKey::PadRB},
+      {XINPUT_GAMEPAD_BACK, GenricKey::PadReturn},
+      {XINPUT_GAMEPAD_START, GenricKey::PadMenu},
+      {XINPUT_GAMEPAD_LEFT_THUMB, GenricKey::StickLeft},
+      {XINPUT_GAMEPAD_RIGHT_THUMB, GenricKey::StickRight},
+      // Keyboard Letters
+      {'A', GenricKey::A},
+      {'B', GenricKey::B},
+      {'C', GenricKey::C},
+      {'D', GenricKey::D},
+      {'E', GenricKey::E},
+      {'F', GenricKey::F},
+      {'G', GenricKey::G},
+      {'H', GenricKey::H},
+      {'I', GenricKey::I},
+      {'J', GenricKey::J},
+      {'K', GenricKey::K},
+      {'L', GenricKey::L},
+      {'M', GenricKey::M},
+      {'N', GenricKey::N},
+      {'O', GenricKey::O},
+      {'P', GenricKey::P},
+      {'Q', GenricKey::Q},
+      {'R', GenricKey::R},
+      {'S', GenricKey::S},
+      {'T', GenricKey::T},
+      {'U', GenricKey::U},
+      {'V', GenricKey::V},
+      {'W', GenricKey::W},
+      {'X', GenricKey::X},
+      {'Y', GenricKey::Y},
+      {'Z', GenricKey::Z},
+      // Keyboard Numbers
+      {'0', GenricKey::Num0},
+      {'1', GenricKey::Num1},
+      {'2', GenricKey::Num2},
+      {'3', GenricKey::Num3},
+      {'4', GenricKey::Num4},
+      {'5', GenricKey::Num5},
+      {'6', GenricKey::Num6},
+      {'7', GenricKey::Num7},
+      {'8', GenricKey::Num8},
+      {'9', GenricKey::Num9},
+      // Function Keys
+      {VK_F1, GenricKey::F1},
+      {VK_F2, GenricKey::F2},
+      {VK_F3, GenricKey::F3},
+      {VK_F4, GenricKey::F4},
+      {VK_F5, GenricKey::F5},
+      {VK_F6, GenricKey::F6},
+      {VK_F7, GenricKey::F7},
+      {VK_F8, GenricKey::F8},
+      {VK_F9, GenricKey::F9},
+      {VK_F10, GenricKey::F10},
+      {VK_F11, GenricKey::F11},
+      {VK_F12, GenricKey::F12},
+      // Symbols
+      {VK_OEM_3, GenricKey::Backtick},        // `~ key
+      {VK_OEM_MINUS, GenricKey::Minus},       // -_ key
+      {VK_OEM_PLUS, GenricKey::Equals},       // =+ key
+      {VK_OEM_4, GenricKey::BracketLeft},     // [{ key
+      {VK_OEM_6, GenricKey::BracketRight},    // ]} key
+      {VK_OEM_5, GenricKey::Backslash},       // \| key
+      {VK_OEM_1, GenricKey::Semicolon},       // ;: key
+      {VK_OEM_7, GenricKey::Quote},           // '" key
+      {VK_OEM_COMMA, GenricKey::Comma},       // ,< key
+      {VK_OEM_PERIOD, GenricKey::Period},     // .> key
+      {VK_OEM_2, GenricKey::Slash},           // /? key
+      // Control Keys
+      {VK_ESCAPE, GenricKey::Esc},
+      {VK_TAB, GenricKey::Tab},
+      {VK_CAPITAL, GenricKey::CapsLock},
+      {VK_SHIFT, GenricKey::Shift},           // Covers both left and right Shift
+      {VK_CONTROL, GenricKey::Ctrl},          // Covers both left and right Ctrl
+      {VK_MENU, GenricKey::Alt},              // Covers both left and right Alt
+      {VK_SPACE, GenricKey::Space},
+      {VK_BACK, GenricKey::Backspace},
+      {VK_RETURN, GenricKey::Enter},
+      // Arrow Keys
+      {VK_UP, GenricKey::ArrowUp},
+      {VK_DOWN, GenricKey::ArrowDown},
+      {VK_LEFT, GenricKey::ArrowLeft},
+      {VK_RIGHT, GenricKey::ArrowRight}
+   };
+
+   HWND hwnd{};
+
+   std::u16string inputBuffer{};
+   XMFLOAT2 micePos{};
+   XMFLOAT2 miceOffset{};
+   float wheelOffset{};
+
+#elif defined(__ANDROID__)
+   //...
+#endif
+
+   std::map<GenricKey, State> genericKeyStates{};
 }
 
 namespace Pillow::Input
@@ -34,7 +155,7 @@ namespace Pillow::Input
    void InputPreTick()
    {
       // Update key states.
-      for (auto& keyState : keyStates)
+      for (auto& keyState : genericKeyStates)
       {
          if (keyState.second == State::Up)
          {
@@ -83,17 +204,17 @@ namespace Pillow::Input
 
    bool GetKey(GenricKey key)
    {
-      return keyStates[key] == State::Pressed || keyStates[key] == State::Down;
+      return genericKeyStates[key] == State::Pressed || genericKeyStates[key] == State::Down;
    }
 
    bool GetKeyDown(GenricKey key)
    {
-      return keyStates[key] == State::Down;
+      return genericKeyStates[key] == State::Down;
    }
 
    bool GetKeyUp(GenricKey key)
    {
-      return keyStates[key] == State::Up;
+      return genericKeyStates[key] == State::Up;
    }
 
    std::u32string GetInputStringAndClear()
