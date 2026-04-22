86898f4864cacba513068cb25132d0e14a952638
Pillow's Tiny Desktop <109895040+ModulePillow@users.noreply.github.com>
2026-04-22 11:55:27 +0800

    Add class Camera

--- SourceCode/Pillow/World.cc
+++ SourceCode/Pillow/World.cc
@@ -3,5 +3,25 @@
 
 namespace Pillow::World
 {
-
+   void Camera::Write(Graphics::PassConstantBuffer& passCB)
+   {
+      // Matrix View
+      XMVECTOR pos = XMLoadFloat4A(&Position);
+      XMVECTOR dir = XMVector3Rotate(XMVectorSet(0, 0, 1, 0), XMLoadFloat4A(&Quaternion));
+      XMVECTOR up = XMVector3Rotate(XMVectorSet(0, 1, 0, 0), XMLoadFloat4A(&Quaternion));
+      XMMATRIX matrixView = XMMatrixLookToLH(pos, dir, up);
+      XMStoreFloat3x4A(&passCB.MatrixView, matrixView);
+      // Matrix Projection
+      XMMATRIX matrixProj = XMMatrixPerspectiveFovLH(Config.VerticalFOV, Config.AspectRatio, Config.NearZ, Config.FarZ);
+      XMStoreFloat4x4A(&passCB.MatrixProj, matrixProj);
+      // Matrix View-Projection
+      XMMATRIX matrixViewProj = XMMatrixMultiply(matrixProj, matrixView);
+      XMStoreFloat4x4A(&passCB.MatrixViewProjection, matrixViewProj);
+      // Matrix Inverse View-Projection
+      XMMATRIX matrixViewProjInv = XMMatrixInverse(nullptr, matrixViewProj);
+      // Other information
+      passCB.CameraPositionWorld = {Position.x, Position.y, Position.z};
+      passCB.DistanceNear = Config.NearZ;
+      passCB.DistanceFar = Config.FarZ;
+   }
 }
--- SourceCode/Pillow/World.h
+++ SourceCode/Pillow/World.h
@@ -1,17 +1,41 @@
 // PillowBasics Copyright (c) 2025, Icey Pillow. BSD 3-Clause License. Do not remove, obscure, or alter this notice.
 #pragma once
 #include <vector>
+#include "DirectXMath-apr2025/DirectXMath.h"
+#include "Renderers/Renderer.h"
+
+using namespace DirectX;
+
 namespace Pillow::World
 {
    using ComponentEvent = void(*)(float deltaTime);
+   class IGameObject;
 
-   class GameObject
+   // Multi-threaded manager for game objects.
+   class GameObjectMTManager
    {
+
+   };
+
+   class IGameObject
+   {
+   public:
+      XMFLOAT4A Position;
+      XMFLOAT4A Quaternion;
       std::vector<ComponentEvent> ticks{};
    };
 
-   class GenericCamera : public GameObject
+   class Camera : public IGameObject
    {
+   public:
+      struct Configuration
+      {
+         float VerticalFOV;
+         float AspectRatio;
+         float NearZ;
+         float FarZ;
+      } Config;
 
+      void Write(Graphics::PassConstantBuffer& passCB);
    };
 }
