fa7666cb03c02ba9cb6af5edae29c8b793a27630
Pillow's Tiny Desktop <109895040+ModulePillow@users.noreply.github.com>
2026-05-07 23:54:04 +0800

    Add gameplay entry logic

--- SourceCode/Gameplay/GameplayEntry.cc
+++ SourceCode/Gameplay/GameplayEntry.cc
@@ -27,21 +27,31 @@ namespace Gameplay
    // Game logic goes here.
    void BaseTick()
    {
+      // Parameters.
       double deltaTime = GetFrameDeltaTime();
       double lapseTime = GetLapseTimeSinceLaunch();
       float safeLapseTime = (float)std::fmod(lapseTime, Constants::TimeLapseMax);
       uint64_t frameIdx = renderer->GetFrameIndex();
       uint32_t safeFrameIdx = (uint32_t)(frameIdx % Constants::FrameIdxMax);
 
-      // Camera.
-
       // Clear the main render target.
-      XMVECTOR _color = XMVectorReplicate(lapseTime);
-      _color = XMVectorAdd(_color, XMVectorSet(0, XM_PI * 0.66f, XM_PI * 1.33f, 0));
-      _color = XMVectorMultiplyAdd(XMVectorSin(_color), XMVectorReplicate(0.5f), XMVectorReplicate(0.5f));
-      XMFLOAT4 color;
-      XMStoreFloat4(&color, _color);
-      PipelineBuffer buffers[1] = { PipelineBuffer::BackBuffer };
-      CmdClearPiplelineBuffers(buffers, 1, color);
+      //XMVECTOR _color = XMVectorReplicate(lapseTime);
+      //_color = XMVectorAdd(_color, XMVectorSet(0, XM_PI * 0.66f, XM_PI * 1.33f, 0));
+      //_color = XMVectorMultiplyAdd(XMVectorSin(_color), XMVectorReplicate(0.5f), XMVectorReplicate(0.5f));
+      //XMFLOAT4 color;
+      //XMStoreFloat4(&color, _color);
+      //PipelineBuffer buffers[1] = { PipelineBuffer::BackBuffer };
+      //CmdClearPiplelineBuffers(buffers, 1, color);
+
+      //Clear render targets.
+      PipelineBuffer buffers1[4] = { PipelineBuffer::Depth, PipelineBuffer::BackBuffer, PipelineBuffer::Buffer1, PipelineBuffer::Buffer2 };
+      CmdClearPiplelineBuffers(buffers1, 4, DefaultBackColor);
+
+      // Draw a bunny.
+      PipelineBuffer buffers2[3] = { PipelineBuffer::Depth, PipelineBuffer::Buffer1, PipelineBuffer::Buffer2 };
+      CmdSetPipelineBuffers(buffers2, 3);
+      //CmdSetActiveCamera();
+      
+      CmdDispatchMesh(0, 1);
    }
 }
