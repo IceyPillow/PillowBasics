50bb4c5a9907b5d21c396f61d6ebd97355dc4aab
Pillow's Tiny Desktop <109895040+ModulePillow@users.noreply.github.com>
2026-05-16 23:49:12 +0800

    BuiltInGeometry.h: Seperate static storage data from a *.cc file

--- SourceCode/Pillow/Resources/Mesh.cc
+++ SourceCode/Pillow/Resources/Mesh.cc
@@ -4,71 +4,7 @@
 #include "cgltf-1.15/cgltf.h"
 #undef CGLTF_IMPLEMENTATION
 #include "Mesh.h"
-
-using namespace Pillow::Graphics;
-
-namespace
-{
-   constexpr float x = 1.0f;
-
-   constexpr float hx = 0.5f;
-
-   constexpr float r = 0.5f;
-
-   constexpr StandardVertex QuadV[4] =
-   {
-      { XMFLOAT4A{-hx,0,-hx,0}, {}, {}, {0.f,0.f,0.f,0.f}, {}, {0.f,1.f,0.f,0.f},{1.f,0.f,0.f,0.f} },
-      { XMFLOAT4A{-hx,0, hx,0}, {}, {}, {0.f,1.f,0.f,0.f}, {}, {0.f,1.f,0.f,0.f},{1.f,0.f,0.f,0.f} },
-      { XMFLOAT4A{ hx,0, hx,0}, {}, {}, {1.f,1.f,0.f,0.f}, {}, {0.f,1.f,0.f,0.f},{1.f,0.f,0.f,0.f} },
-      { XMFLOAT4A{ hx,0,-hx,0}, {}, {}, {1.f,0.f,0.f,0.f}, {}, {0.f,1.f,0.f,0.f},{1.f,0.f,0.f,0.f} }
-   };
-
-   constexpr uint32_t QuadI[6] = { 0, 1, 2, 0, 2, 3};
-
-   constexpr StandardVertex CubeV[24] =
-   {
-      // Front
-      { XMFLOAT4A{-hx,-hx,-hx,0}, {}, {}, {0.f,0.f,0.f,0.f}, {}, {0.f,0.f,-1.f,0.f},{1.f,0.f,0.f,0.f} },
-      { XMFLOAT4A{-hx, hx,-hx,0}, {}, {}, {0.f,1.f,0.f,0.f}, {}, {0.f,0.f,-1.f,0.f},{1.f,0.f,0.f,0.f} },
-      { XMFLOAT4A{ hx, hx,-hx,0}, {}, {}, {1.f,1.f,0.f,0.f}, {}, {0.f,0.f,-1.f,0.f},{1.f,0.f,0.f,0.f} },
-      { XMFLOAT4A{ hx,-hx,-hx,0}, {}, {}, {1.f,0.f,0.f,0.f}, {}, {0.f,0.f,-1.f,0.f},{1.f,0.f,0.f,0.f} },
-      // Back
-      { XMFLOAT4A{ hx,-hx, hx,0}, {}, {}, {0.f,0.f,0.f,0.f}, {}, {0.f,0.f,1.f,0.f},{-1.f,0.f,0.f,0.f} },
-      { XMFLOAT4A{ hx, hx, hx,0}, {}, {}, {0.f,1.f,0.f,0.f}, {}, {0.f,0.f,1.f,0.f},{-1.f,0.f,0.f,0.f} },
-      { XMFLOAT4A{-hx, hx, hx,0}, {}, {}, {1.f,1.f,0.f,0.f}, {}, {0.f,0.f,1.f,0.f},{-1.f,0.f,0.f,0.f} },
-      { XMFLOAT4A{-hx,-hx, hx,0}, {}, {}, {1.f,0.f,0.f,0.f}, {}, {0.f,0.f,1.f,0.f},{-1.f,0.f,0.f,0.f} },
-      // Left
-      { XMFLOAT4A{-hx,-hx, hx,0}, {}, {}, {0.f,0.f,0.f,0.f}, {}, {-1.f,0.f,0.f,0.f},{0.f,0.f,-1.f,0.f} },
-      { XMFLOAT4A{-hx, hx, hx,0}, {}, {}, {0.f,1.f,0.f,0.f}, {}, {-1.f,0.f,0.f,0.f},{0.f,0.f,-1.f,0.f} },
-      { XMFLOAT4A{-hx, hx,-hx,0}, {}, {}, {1.f,1.f,0.f,0.f}, {}, {-1.f,0.f,0.f,0.f},{0.f,0.f,-1.f,0.f} },
-      { XMFLOAT4A{-hx,-hx,-hx,0}, {}, {}, {1.f,0.f,0.f,0.f}, {}, {-1.f,0.f,0.f,0.f},{0.f,0.f,-1.f,0.f} },
-      // Right
-      { XMFLOAT4A{ hx,-hx,-hx,0}, {}, {}, {0.f,0.f,0.f,0.f}, {}, {1.f,0.f,0.f,0.f},{0.f,0.f,1.f,0.f} },
-      { XMFLOAT4A{ hx, hx,-hx,0}, {}, {}, {0.f,1.f,0.f,0.f}, {}, {1.f,0.f,0.f,0.f},{0.f,0.f,1.f,0.f} },
-      { XMFLOAT4A{ hx, hx, hx,0}, {}, {}, {1.f,1.f,0.f,0.f}, {}, {1.f,0.f,0.f,0.f},{0.f,0.f,1.f,0.f} },
-      { XMFLOAT4A{ hx,-hx, hx,0}, {}, {}, {1.f,0.f,0.f,0.f}, {}, {1.f,0.f,0.f,0.f},{0.f,0.f,1.f,0.f} },
-      // Top
-      { XMFLOAT4A{-hx, hx,-hx,0}, {}, {}, {0.f,0.f,0.f,0.f}, {}, {0.f,1.f,0.f,0.f},{1.f,0.f,0.f,0.f} },
-      { XMFLOAT4A{-hx, hx, hx,0}, {}, {}, {0.f,1.f,0.f,0.f}, {}, {0.f,1.f,0.f,0.f},{1.f,0.f,0.f,0.f} },
-      { XMFLOAT4A{ hx, hx, hx,0}, {}, {}, {1.f,1.f,0.f,0.f}, {}, {0.f,1.f,0.f,0.f},{1.f,0.f,0.f,0.f} },
-      { XMFLOAT4A{ hx, hx,-hx,0}, {}, {}, {1.f,0.f,0.f,0.f}, {}, {0.f,1.f,0.f,0.f},{1.f,0.f,0.f,0.f} },
-      // Bottom
-      { XMFLOAT4A{-hx,-hx, hx,0}, {}, {}, {0.f,0.f,0.f,0.f}, {}, {0.f,-1.f,0.f,0.f},{1.f,0.f,0.f,0.f} },
-      { XMFLOAT4A{-hx,-hx,-hx,0}, {}, {}, {0.f,1.f,0.f,0.f}, {}, {0.f,-1.f,0.f,0.f},{1.f,0.f,0.f,0.f} },
-      { XMFLOAT4A{ hx,-hx,-hx,0}, {}, {}, {1.f,1.f,0.f,0.f}, {}, {0.f,-1.f,0.f,0.f},{1.f,0.f,0.f,0.f} },
-      { XMFLOAT4A{ hx,-hx, hx,0}, {}, {}, {1.f,0.f,0.f,0.f}, {}, {0.f,-1.f,0.f,0.f},{1.f,0.f,0.f,0.f} }
-   };
-
-   constexpr uint32_t CubeI[36] =
-   {
-        0, 1, 2, 0, 2, 3,
-        4, 5, 7, 5, 6, 7,
-        8, 9,10, 8,10,11,
-       12,13,15,13,14,15,
-       16,17,18,16,18,19,
-       20,21,23,21,22,23,
-   };
-}
+#include "BuiltInGeometry.h"
 
 namespace Pillow::Graphics
 {
