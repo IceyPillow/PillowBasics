0f7c6c4383b0f58ef72c18faaaabef0e678878e9
Pillow's Tiny Desktop <109895040+ModulePillow@users.noreply.github.com>
2026-06-12 15:09:31 +0800

    Update the generation batch file: Now support generator choosing!

--- Make.bat
+++ Make.bat
@@ -1,24 +1,35 @@
 @echo off
 setlocal
 
-:: Delete stale CMake files.
+echo.
+echo [Arguments Help]
+echo.
+echo 26: Choose Visual Studio 2026. (VS 2022 is default)
+echo -c: Delete the CMake-generated files.
+echo.
+echo.
+
 set CLEAN=false
+set Generator="Visual Studio 17 2022"
+set SolutionEntry="PillowBasics.sln"
 for %%i in (%*) do (
     if "%%i"=="-c" set CLEAN=true
+    if "%%i"=="26" (
+      set Generator = "Visual Studio 18 2026"
+      set SolutionEntry="PillowBasics.slnx"
+    )
 )
-echo.
+:: Delete stale CMake files.
 if "%CLEAN%"=="true" (
     echo Deleting stale CMake files...
     rd /s/q Cmake
-) else (
-    echo Tip: Add "-c" to clean stale CMake files forcely.
 )
 echo.
 
 :: Generate.
-cmake -G "Visual Studio 18 2026" -DCMAKE_TOOLCHAIN_FILE=./ToolchainWin64.cmake -S ./SourceCode -B ./Cmake/Win64
+cmake -G %Generator% -DCMAKE_TOOLCHAIN_FILE=./ToolchainWin64.cmake -S ./SourceCode -B ./Cmake/Win64
 
 :: Open the solution.
 echo.
 set /p input=Open the solution? (y/n)
-if /i "%input%"=="y" start ./Cmake/Win64/PillowBasics.slnx
+if /i "%input%"=="y" start ./Cmake/Win64/%SolutionEntry%
