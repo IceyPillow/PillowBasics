0343aa6f3cb2c8b2a4645fd7134cfce6f0a8bede
Pillow's Tiny Desktop <109895040+ModulePillow@users.noreply.github.com>
2026-07-31 16:50:48 +0800

    Revise ApplyBarrier()

--- SourceCode/Pillow/Renderers/D3D12Renderer.cc
+++ SourceCode/Pillow/Renderers/D3D12Renderer.cc
@@ -219,7 +219,8 @@ namespace
 {
    void Check_HRESULT(HRESULT hResult);
    void ApplyBarrier(ComPtr<ICommandList>& cmdList, ComPtr<IResource>& resource,
-      D3D12_RESOURCE_STATES before, D3D12_RESOURCE_STATES after);
+      D3D12_RESOURCE_STATES before, D3D12_RESOURCE_STATES after,
+      uint32_t subRes = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES);
    uint32_t GetAlignMipmapSize(const TextureInfo& texInfo, uint32_t mipLevel);
    uint32_t GetAlignTextureArraySliceSize(const TextureInfo& texInfo);
 
@@ -1364,15 +1365,13 @@ namespace
 
    // Granularity up to subresource level.
    void ApplyBarrier(ComPtr<ICommandList>& cmdList, ComPtr<IResource>& resource,
-      D3D12_RESOURCE_STATES before, D3D12_RESOURCE_STATES after)
       D3D12_RESOURCE_STATES before, D3D12_RESOURCE_STATES after,
-      uint32_t subRes = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES)
+      uint32_t subRes /*= D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES*/)
    {
       D3D12_RESOURCE_BARRIER barrier
       {
          D3D12_RESOURCE_BARRIER_TYPE_TRANSITION,
          D3D12_RESOURCE_BARRIER_FLAG_NONE,
-         D3D12_RESOURCE_TRANSITION_BARRIER { resource.Get(), D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES, before, after}
          D3D12_RESOURCE_TRANSITION_BARRIER { resource.Get(), subRes, before, after}
       };
       cmdList->ResourceBarrier(1, &barrier);
